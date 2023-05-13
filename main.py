c79a074373e188e69fc47f72ac5e705e4b2e633c
Ricardo Contreras <ricardo.contreras568@hotmail.com>
2023-05-12 23:07:59 -0500

    Se hizo una primera prueba de la aplicación y se definieron solo las librerias importante en el archivo requirements.txt

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-#Importaciones
-import tkinter as tk
+import tkinter
+import customtkinter
 
-#Creación de la ventana principal
-root = tk.Tk()
+customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
-#Creación de la etiqueta
-mensaje = tk.Label(root, text="Mi primer programa con Tkinter.")
-#Muestra la etiqueta
-mensaje.pack()
+app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+app.geometry("400x240")
 
-#Bucle de ejecución
-root.mainloop()
+def button_function():
+    print("button pressed")
+
+# Use CTkButton instead of tkinter Button
+button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
+button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+app.mainloop()
