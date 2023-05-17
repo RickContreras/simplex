fc3934255aed6bb2cfc0f90ccd25cf2fe6332e79
Ricardo Contreras <ricardo.contreras568@hotmail.com>
2023-05-16 21:58:44 -0500

    Se hizo la base del programa que genera una ventana adicional para introduccir los datos para el solucionar el problema.

--- main.py
+++ main.py
@@ -1,17 +1,84 @@
-import tkinter
-import customtkinter
+import tkinter as tk
+import customtkinter as ctk
 
-customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+def generar_filas_columnas():
+    num_filas = int(entrada_filas.get())
+    num_columnas = int(entrada_columnas.get())
+    
+    # Ocultar los widgets anteriores
+    # label_filas.pack_forget()
+    # entrada_filas.pack_forget()
+    # label_columnas.pack_forget()
+    # entrada_columnas.pack_forget()
+    # boton_generar.pack_forget()
+    
+    # Crear nueva ventana
+    nueva_ventana = tk.Toplevel(ventana)
+    nueva_ventana.title("Simplex Aumentado")
+    
+    # Crear marco en la nueva ventana
+    frame_nueva_ventana = tk.Frame(nueva_ventana)
+    frame_nueva_ventana.pack(padx=10, pady=10)
 
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-app.geometry("400x240")
+    for i in range(num_filas+2):
+        for j in range(num_columnas+3):
 
-def button_function():
-    print("button pressed")
+            
 
-# Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
-button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+            if (i==0):
 
-app.mainloop()
+                if(j==0):
+                    label = tk.Label(frame_nueva_ventana, text="Restricciones\\Variables")
+                    label.grid(row=0, column=0, padx=5, pady=5)
+                elif(j==num_columnas+1):
+                    label = tk.Label(frame_nueva_ventana, text="Simbolo")
+                    label.grid(row=0, column=num_columnas+1, padx=5, pady=5)
+                elif(j==num_columnas+2):
+                    label = tk.Label(frame_nueva_ventana, text="b")
+                    label.grid(row=0, column=num_columnas+2, padx=5, pady=5)
+                else:
+                    label = tk.Label(frame_nueva_ventana, text=f"X{j}")
+                    label.grid(row=0, column=j, padx=5, pady=5)
+
+                    
+            else:
+                if (j==0):
+                    
+                    label = tk.Label(frame_nueva_ventana, text=f"Ecuación {i-1}:")
+                    label.grid(row=i, column=j, padx=5, pady=5)
+                elif(j==num_columnas+1):
+                    label = tk.Label(frame_nueva_ventana, text="=")
+                    label.grid(row=i, column=j, padx=5, pady=5)
+                else:
+                    entry = ctk.CTkEntry(frame_nueva_ventana)
+                    entry.grid(row=i, column=j, padx=5, pady=5)
+
+
+
+# Crear ventana
+ventana = tk.Tk()
+ventana.title("Metodo Simplex")
+
+# Crear marco
+frame = tk.Frame(ventana)
+frame.pack(padx=10, pady=10)
+
+# Crear etiquetas y casillas de texto para introducir el número de filas y columnas
+label_filas = tk.Label(frame, text="Introduce el numero de restricciones:")
+label_filas.pack(padx=5, pady=5)
+
+entrada_filas = ctk.CTkEntry(frame)
+entrada_filas.pack(padx=5, pady=5)
+
+label_columnas = tk.Label(frame, text="Introduce el número de variables:")
+label_columnas.pack(padx=5, pady=5)
+
+entrada_columnas = ctk.CTkEntry(frame)
+entrada_columnas.pack(padx=5, pady=5)
+
+# Botón para generar filas y columnas
+boton_generar = tk.Button(frame, text="Generar problema", command=generar_filas_columnas)
+boton_generar.pack(padx=5, pady=5)
+
+# Ejecutar ventana
+ventana.mainloop()
